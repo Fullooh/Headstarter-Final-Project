1098c4dfca128cd7b416464b7b47121385b9dfc8
Fullooh <angstdemon1@aol.com>
2024-08-18 02:25:57 +0300

    Added services folder and Profiles section, worked on UI

--- syncup/app/services/profileService.js
+++ syncup/app/services/profileService.js
@@ -2,9 +2,9 @@
 import { firestore } from './firebase'; // Import the firestore instance
 import { collection, getDocs } from 'firebase/firestore';
 
-// Function to fetch all user profiles from the "profiles" collection
+// Function to fetch all user profiles from the "users" collection
 export const fetchUserProfiles = async () => {
-  const querySnapshot = await getDocs(collection(firestore, 'profiles'));
+  const querySnapshot = await getDocs(collection(firestore, 'users')); // Fetch from 'users' collection
   const profiles = querySnapshot.docs.map(doc => ({
     id: doc.id,
     ...doc.data(),
