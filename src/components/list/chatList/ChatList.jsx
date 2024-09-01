6ba8769a093415188e75959eeea681905a63bae5
JKKO0 <jasur.halilov123@gmail.com>
2024-08-31 21:07:11 -0400

    added minor changes

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -16,23 +16,23 @@ const ChatList = () => {
 
   useEffect(() => {
     const unSub = onSnapshot(
-      doc(db, "userchats", currentUser.id),
-      async (res) => {
-        const items = res.data().chats;
+        doc(db, "userchats", currentUser.id),
+        async (res) => {
+          const items = res.data().chats;
 
-        const promises = items.map(async (item) => {
-          const userDocRef = doc(db, "users", item.receiverId);
-          const userDocSnap = await getDoc(userDocRef);
+          const promises = items.map(async (item) => {
+            const userDocRef = doc(db, "users", item.receiverId);
+            const userDocSnap = await getDoc(userDocRef);
 
-          const user = userDocSnap.data();
+            const user = userDocSnap.data();
 
-          return { ...item, user };
-        });
+            return { ...item, user };
+          });
 
-        const chatData = await Promise.all(promises);
+          const chatData = await Promise.all(promises);
 
-        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
-      }
+          setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+        }
     );
 
     return () => {
@@ -47,7 +47,7 @@ const ChatList = () => {
     });
 
     const chatIndex = userChats.findIndex(
-      (item) => item.chatId === chat.chatId
+        (item) => item.chatId === chat.chatId
     );
 
     userChats[chatIndex].isSeen = true;
@@ -65,57 +65,57 @@ const ChatList = () => {
   };
 
   const filteredChats = chats.filter((c) =>
-    c.user.username.toLowerCase().includes(input.toLowerCase())
+      c.user.username.toLowerCase().includes(input.toLowerCase())
   );
 
   return (
-    <div className="chatList">
-      <div className="search">
-        <div className="searchBar">
-          <img src="./search.png" alt="" />
-          <input
-            type="text"
-            placeholder="Search"
-            onChange={(e) => setInput(e.target.value)}
+      <div className="chatList">
+        <div className="search">
+          <div className="searchBar">
+            <img src="./search.png" alt="" />
+            <input
+                type="text"
+                placeholder="Search"
+                onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
+          <img
+              src={addMode ? "./minus.png" : "./plus.png"}
+              alt=""
+              className="add"
+              onClick={() => setAddMode((prev) => !prev)}
           />
         </div>
-        <img
-          src={addMode ? "./minus.png" : "./plus.png"}
-          alt=""
-          className="add"
-          onClick={() => setAddMode((prev) => !prev)}
-        />
-      </div>
-      {filteredChats.map((chat) => (
-        <div
-          className="item"
-          key={chat.chatId}
-          onClick={() => handleSelect(chat)}
-          style={{
-            backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
-          }}
-        >
-          <img
-            src={
-              chat.user.blocked.includes(currentUser.id)
-                ? "./avatar.png"
-                : chat.user.avatar || "./avatar.png"
-            }
-            alt=""
-          />
-          <div className="texts">
+        {filteredChats.map((chat) => (
+            <div
+                className="item"
+                key={chat.chatId}
+                onClick={() => handleSelect(chat)}
+                style={{
+                  backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
+                }}
+            >
+              <img
+                  src={
+                    chat.user.blocked.includes(currentUser.id)
+                        ? "./avatar.png"
+                        : chat.user.avatar || "./avatar.png"
+                  }
+                  alt=""
+              />
+              <div className="texts">
             <span>
               {chat.user.blocked.includes(currentUser.id)
-                ? "User"
-                : chat.user.username}
+                  ? "User"
+                  : chat.user.username}
             </span>
-            <p>{chat.lastMessage}</p>
-          </div>
-        </div>
-      ))}
+                <p>{chat.lastMessage}</p>
+              </div>
+            </div>
+        ))}
 
-      {addMode && <AddUser />}
-    </div>
+        {addMode && <AddUser onClose={() => setAddMode(false)} />}
+      </div>
   );
 };
 
